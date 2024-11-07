24bed1105cc3d92cf4815431ba63ffdbcfca9582
Rommel <rrommel5@gmail.com>
2024-11-07 10:32:27 +0100

    images

--- app/src/Controller/PageController.php
+++ app/src/Controller/PageController.php
@@ -15,19 +15,4 @@ class PageController extends AbstractController
         return $this->render('page/tinder.html.twig', []);
     }
 
-    private $images = ["images/image2.jpg", "images/image3.jpg", "images/image4.jpg", "images/image1.jpg"];
-    private $currentIndex = 0;
-    #[Route('/tinder/image', name: 'app_tinder_image')]
-    public function getImage(): JsonResponse
-    {
-        if ($this->currentIndex >= count($this->images)) {
-            $this->currentIndex = 0;
-        }
-
-        //$imagePath = $this->images[$this->currentIndex];
-        $imagePath = $this->generateUrl($this->images[$this->currentIndex]);
-        $this->currentIndex++;
-        return new JsonResponse(['image' => $imagePath]);
-
-    }
 }
