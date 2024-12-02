c9571140f42f60880f89dda288c435ab7772cae9
Rommel <rrommel5@gmail.com>
2024-12-02 09:20:49 +0100

    likes (que no muestre los likes de los match)

--- app/src/Controller/LikesController.php
+++ app/src/Controller/LikesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PairRepository;
 use App\Repository\SwipeRepository;
 use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -12,13 +13,56 @@ use Symfony\Component\Routing\Annotation\Route;
 class LikesController extends AbstractController
 {
     #[Route('/likes', name: 'app_likes')]
-    public function likes(SwipeRepository $swipeRepository): Response
+    public function likes(SwipeRepository $swipeRepository, PairRepository $pairRepository): Response
     {
-        $user = $this->getUser();
+        /*$user = $this->getUser();
         $likes = $swipeRepository->findBy(['userB' => $user, 'action' => '1']);
         return $this->render('likes/index.html.twig', [
             'likes' => $likes,
-        ]);
+        ]);*/
 
+        $user = $this->getUser();
+
+        /*$usersMatched = $swipeRepository->createQueryBuilder('p')
+            ->select('IDENTITY(p.userB)')
+            ->where('p.userA = :user')
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getResult();
+
+        $excludeIds = array_column($usersMatched, 1);
+
+        $likes = $swipeRepository->createQueryBuilder('l')
+            ->where('l.userB = :user')
+            ->andWhere('l.action = true')
+            ->andWhere('l.userA NOT IN (:excludeIds)')
+            ->setParameter('user', $user)
+            ->setParameter('excludeIds', $excludeIds ?: [0])
+            ->getQuery()
+            ->getResult();*/
+
+        $usersMatched = $pairRepository->createQueryBuilder('p')
+            ->select('IDENTITY(p.userB)')
+            ->where('p.userA = :user')
+            ->orWhere('p.userB = :user')
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getResult();
+
+        $excludeIds = array_column($usersMatched, 1);
+
+        $likes = $swipeRepository->createQueryBuilder('l')
+            ->where('l.userB = :user')
+            ->andWhere('l.action = true')
+            ->andWhere('l.userA NOT IN (:excludeIds)')
+            ->setParameter('user', $user)
+            ->setParameter('excludeIds', $excludeIds ?: [0])
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('likes/index.html.twig', [
+            'likes' => $likes,
+        ]);
     }
+
 }
