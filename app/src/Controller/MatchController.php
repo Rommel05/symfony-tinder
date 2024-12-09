ae8e8da65572d8b1efa56032e2925f41d2b6739a
Rommel <rrommel5@gmail.com>
2024-12-09 09:12:34 +0100

    buscador

--- app/src/Controller/MatchController.php
+++ app/src/Controller/MatchController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
 use App\Repository\PairRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MatchController extends AbstractController
 {
     #[Route('/match', name: 'app_match')]
-    public function match(PairRepository $pairRepository): Response
+    public function match(PairRepository $pairRepository, Request $request): Response
     {
         $user = $this->getUser();
-        $pairs = $pairRepository->findBy(['userA' => $user]);
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+        $pairs = [];
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $searchParameter = $form->get('search')->getData();
+            $pairs = $pairRepository->findBySearchParameter($user,$searchParameter);
+        } else {
+            $pairs = $pairRepository->findBy(['userA' => $user]);
+        }
+
         return $this->render('match/index.html.twig', [
+            'form' => $form->createView(),
             'pairs' => $pairs,
         ]);
     }
