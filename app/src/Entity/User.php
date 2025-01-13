bb1b0a1453e4bbba3ce406ad5514e2c998e3b997
Rommel <rrommel5@gmail.com>
2025-01-13 13:29:25 +0100

    validaciones

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
@@ -20,6 +21,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(message: 'Please enter your email')]
+    #[Assert\Email(message: 'Please enter a valid email')]
     private ?string $email = null;
 
     #[ORM\Column]
--- app/src/Form/RegistrationFormType.php
+++ app/src/Form/RegistrationFormType.php
@@ -19,27 +19,57 @@ use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use function Sodium\add;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('age', TextType::class)
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your name'
+                    ]),
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your email'
+                    ])
+                ]
+            ])
+            ->add('age', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your age'
+                    ]),
+                    new Length([
+                        'min' => 18,
+                    ])
+                ]
+            ])
             ->add('description', TextareaType::class)
             ->add('gender', ChoiceType::class, [
                 'choices' => [
                     'man' => 'man',
                     'woman' => 'woman',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please chose your gender'
+                    ])
                 ]
             ])
             ->add('interests', ChoiceType::class, [
                 'choices' => [
                     'man' => 'man',
                     'woman' => 'woman',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please chose your interests'
+                    ])
                 ]
             ])
             ->add('agreeTerms', CheckboxType::class, [
