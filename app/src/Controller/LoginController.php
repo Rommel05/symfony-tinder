96fd8f2f503f78f826f7f8b6cca8fed516496056
EduVarga <eruk11000@gmail.com>
2025-01-13 08:24:00 +0100

    proyecto

--- app/src/Controller/LoginController.php
+++ app/src/Controller/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    public function index(AuthenticationUtils $authenticationUtils, RequestStack $requestStack): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
