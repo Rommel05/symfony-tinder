5de4c589cdd0e71b922e580154314599c1e61027
Rommel <rrommel5@gmail.com>
2025-01-13 09:52:29 +0100

    barra_busqueda

--- app/src/Controller/MatchController.php
+++ app/src/Controller/MatchController.php
@@ -4,18 +4,37 @@ namespace App\Controller;
 
 use App\Repository\PairRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MatchController extends AbstractController
 {
     #[Route('/match', name: 'app_match')]
-    public function match(PairRepository $pairRepository): Response
+    public function match(PairRepository $pairRepository, Request $request): Response
     {
         $user = $this->getUser();
-        $pairs = $pairRepository->findBy(['userA' => $user]);
+        $form = $this->createFormBuilder()
+            ->add('search', SearchType::class, [
+                'label' => 'Match',
+                'attr' => [
+                    'placeholder' => 'Find your match...',
+                ]
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+        $pairs = [];
+        if ($form->isSubmitted() && $form->isValid()) {
+            $searchParameter = $form->get('search')->getData();
+            $pairs = $pairRepository->findBySearchParameter($user,$searchParameter);
+        } else {
+            $pairs = $pairRepository->findBy(['userA' => $user]);
+        }
+
         return $this->render('match/match.html.twig', [
             'pairs' => $pairs,
+            'form' => $form->createView(),
         ]);
     }
 }
